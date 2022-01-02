e4e82645ddec7fc2c54b5eacdc11ba08229cdc0a
mingtiancai <2427373908@qq.com>
2022-01-02 15:22:16 +0800

    完成猜数字游戏

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(0, 101);
+
+    println!("The secret number is : {}",secret_number);
+    loop{
+        println!("Please input your guess.");
+        let mut guess=String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        // let guess:u32=guess.trim().parse()
+        // .expect("Please type a number!");
+        let guess:u32=match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>{
+                println!("please input a number 0-100!");
+                continue;
+            }
+        };
+        println!("You guessed: {}",guess);
+        match guess.cmp(&secret_number){
+            Ordering::Less=>println!("Too small"),
+            Ordering::Greater=>println!("Too large"),
+            Ordering::Equal=>{
+                println!("you win!");
+                break;
+            }
+        }
+    }
 }
