0df321db25c168cf12c93eeac328000f0f062924
mingtiancai <2427373908@qq.com>
2022-01-13 23:21:31 +0800

    add use tuple and var

--- common_concept/src/main.rs
+++ common_concept/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("the value of x is: {}", x);
+    println!("the value of y is: {}", y);
+    println!("the value of z is: {}", z);
+    println!("the value of x is: {}", tup.0);
+    println!("the value of y is: {}", tup.1);
+    println!("the value of z is: {}", tup.2);
 }
