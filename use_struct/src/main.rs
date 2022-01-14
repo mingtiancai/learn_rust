198ab27402442bc6539b881f621b10774a30368e
mingtiancai <2427373908@qq.com>
2022-01-14 18:04:25 +0800

    use struct

--- use_struct/src/main.rs
+++ use_struct/src/main.rs
@@ -1,3 +1,27 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn print_user(user: &User) {
+    println!("{}", user.username);
+    println!("{}", user.email);
+    println!("{}", user.sign_in_count);
+    println!("{}", user.active);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = build_user(String::from("as"), String::from("ss"));
+    print_user(&user1);
 }
