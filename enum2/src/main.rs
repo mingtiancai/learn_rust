4d999a8a525e34888bbedf109300cadcd94f7102
mingtiancai <2427373908@qq.com>
2022-01-15 20:55:26 +0800

    finish enum2

--- enum2/src/main.rs
+++ enum2/src/main.rs
@@ -10,6 +10,68 @@ struct IpAddr {
     address: String,
 }
 
+#[derive(Debug)]
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn values_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin2 {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin2) -> u32 {
+    match coin {
+        Coin2::Penny => 1,
+        Coin2::Nickel => 5,
+        Coin2::Dime => 10,
+        Coin2::Quarter(state) => {
+            println!("state quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn match_some_value(value: u8) {
+    match value {
+        1 => println!("1"),
+        2 => println!("2"),
+        _ => println!("others"),
+    }
+}
+
 fn main() {
     let home = IpAddr {
         kind: IpAddrKind::V4,
@@ -22,4 +84,22 @@ fn main() {
 
     println!("{:#?}", home);
     println!("{:#?}", loopback);
+
+    let home2 = IpAddr2::V4(String::from("127.0.0.1"));
+    println!("{:#?}", home2);
+
+    // let some_number = Some(5);
+    // let some_number2 = Some(4);
+    // let p = some_number2.clone();
+    // let some_string = Some("a string");
+    // let absent_number: Option<i32> = None;
+    // let pp = values_in_cents(Coin::Quarter);
+    // println!("{}", pp);
+    value_in_cents(Coin2::Quarter(UsState::Alaska));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    let x: u8;
+    x = 1;
+    match_some_value(x);
 }
