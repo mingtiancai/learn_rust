31cba2a7e36f439e1223702dbda52c731fab03aa
mingtiancai <2427373908@qq.com>
2022-01-18 23:35:17 +0800

    mini grep v4

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,25 +8,29 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic! {"not enough arguments"};
+            return Err("not enough arguments");
         } else if args.len() > 3 {
-            panic! {"too much arguments"};
+            return Err("too much arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) {
+    let contents =
+        fs::read_to_string(config.filename).expect("something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    run(config);
 }
