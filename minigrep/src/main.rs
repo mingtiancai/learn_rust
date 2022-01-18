ae592b38975dd24274becb1a854eb01c40ca44c4
mingtiancai <2427373908@qq.com>
2022-01-18 22:08:31 +0800

    minigrep v3

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,22 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic! {"not enough arguments"};
+        } else if args.len() > 3 {
+            panic! {"too much arguments"};
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
