055356091a5962359d5a49c29f805f95f117df89
mingtiancai <2427373908@qq.com>
2022-01-21 20:24:26 +0800

    finish ptr

--- use_ptr/src/main.rs
+++ use_ptr/src/main.rs
@@ -1,5 +1,6 @@
+use std::cell::RefCell;
 use std::ops::Deref;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 struct MyBox<T>(T);
 
@@ -26,11 +27,56 @@ impl Drop for CustomSmartPoint {
     }
 }
 
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning : You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    // let x = 5;
+    // let y = MyBox::new(x);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
     // let c = CustomSmartPoint {
     //     data: String::from("my stuff"),
     // };
@@ -38,11 +84,79 @@ fn main() {
     //     data: String::from("other stuff"),
     // };
     // println!("CustomSmartPointers created!");
-    let p1 = Rc::new(3);
+    // let p1 = Rc::new(3);
+    // {
+    //     let p2 = Rc::clone(&p1);
+    //     println!("{}:{}  {}", p1, *p1, Rc::strong_count(&p1));
+    // }
+
+    // println!("{}:{}  {}", p1, *p1, Rc::strong_count(&p1));
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {} , weak = {} ",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
     {
-        let p2 = Rc::clone(&p1);
-        println!("{}:{}  {}", p1, *p1, Rc::strong_count(&p1));
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!(
+            "branch strong = {},weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!(
+            "leaf strong = {} , weak = {} ",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+    println!(
+        "leaf strong = {} , weak = {} ",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+    // println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
     }
 
-    println!("{}:{}  {}", p1, *p1, Rc::strong_count(&p1));
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
 }
